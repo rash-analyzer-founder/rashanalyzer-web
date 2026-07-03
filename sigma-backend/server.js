5955e178d3b0d03deb84be2b74952c904e97a1e0
rash-analyzer-founder <tasnimeden@gmail.com>
2026-07-03 19:35:13 +0600

    Why????

--- sigma-backend/server.js
+++ sigma-backend/server.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
 const PORT = 3000;
-
-app.use(cors())
 
 // This handles the main route of your website
 app.get('/', (req, res) => {
