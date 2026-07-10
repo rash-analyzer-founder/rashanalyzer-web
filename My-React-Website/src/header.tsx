996287f5d018609b6e5535021bcea80b220c7199
rash-analyzer-founder <tasnimeden@gmail.com>
2026-07-10 11:31:31 +0600

    Fixed the Lucide Bug

--- My-React-Website/src/header.tsx
+++ My-React-Website/src/header.tsx
@@ -4,7 +4,7 @@ import './header.css';
 function Header() {
   return (
     <header className="site-header">
-      <Flask className="header-logo" aria-label="RashAnalyzer logo" role="img" />
+      <FlaskConical className="header-logo" aria-label="RashAnalyzer logo" role="img" />
       <span id="rashanalyzer-title">RashAnalyzer</span>
     </header>
   )
