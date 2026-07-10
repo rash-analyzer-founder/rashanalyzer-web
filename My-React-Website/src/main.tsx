1449af321d7adb40c5feb6720c67f2ce53ae549f
Vercel <vercel[bot]@users.noreply.github.com>
2026-07-10 14:04:38 +0000

    Install and configure Vercel Speed Insights
    This is an automated pull request generated by Vercel to help install [Speed Insights](https://vercel.com/docs/speed-insights) in your application.
    
    Co-authored-by: Vercel <vercel[bot]@users.noreply.github.com>

--- My-React-Website/src/main.tsx
+++ My-React-Website/src/main.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import FruitList from './list.tsx'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <FruitList />
+    <SpeedInsights />
   </StrictMode>
 )
